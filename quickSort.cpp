ff00c8ddf8d736589bc5093b85e958708533ee9c
Ye Liu <liuye.cis@gmail.com>
2023-03-16 19:38:03 -0700

    test

--- quickSort.cpp
+++ quickSort.cpp
@@ -4,14 +4,7 @@
 
 using namespace std;
 
-// write a quick sort algorithm in C++ using lambda expressions
-void quickSort(vector<int> &arr, int left, int right) {
-   if(left < right) {
-      int pivot = partition(arr, left, right);
-      quickSort(arr, left, pivot - 1);
-      quickSort(arr, pivot + 1, right);
-   }
-}
+
 int partition(vector<int> &arr, int left, int right) {
    int pivot = arr[right];
    int i = left - 1;
@@ -24,6 +17,16 @@ int partition(vector<int> &arr, int left, int right) {
    swap(arr[i + 1], arr[right]);
    return i + 1;
 }
+
+// write a quick sort algorithm in C++ using lambda expressions
+void quickSort(vector<int> &arr, int left, int right) {
+   if(left < right) {
+      int pivot = partition(arr, left, right);
+      quickSort(arr, left, pivot - 1);
+      quickSort(arr, pivot + 1, right);
+   }
+}
+
 // test this function
 int main() {
    vector<int> arr = {5, 4, 3, 2, 1};
